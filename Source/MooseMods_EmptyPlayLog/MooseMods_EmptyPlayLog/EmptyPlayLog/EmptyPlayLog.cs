06a078e921eccf64a48b7b5e6ed9f469e23f3608
Jelly <jellypowered@gmail.com>
2020-03-11 19:40:35 -0500

    Release Ready for 1.1

---
+++ Source/MooseMods_EmptyPlayLog/MooseMods_EmptyPlayLog/EmptyPlayLog/EmptyPlayLog.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace EmptyPawnLog
+{ 
+    public static class AddCraftThoughtClearAction
+    {
+        [DebugAction(DebugActionCategories.MapManagement, "Clear Pawn Log...", allowedGameStates =AllowedGameStates.PlayingOnMap)]
+               
+        private static void ClearCraftThoughts()
+        {   
+            Find.PlayLog.AllEntries.Clear();
+        }
+    }
+}
+
--- Source/MooseMods_EmptyPlayLog/MooseMods_EmptyPlayLog/Properties/AssemblyInfo.cs
+++ Source/MooseMods_EmptyPlayLog/MooseMods_EmptyPlayLog/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MooseMods_BanishAnimals")]
+[assembly: AssemblyTitle("Jellypowered_EmptyPawnLog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MooseMods_BanishAnimals")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Jellypowered_EmptyPawnLog")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
